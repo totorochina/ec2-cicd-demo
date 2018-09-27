3f08e025c9004a48d6ec80219794e8309a1d403b
Dave Chen <totorochina@hotmail.com>
2018-09-27 22:54:22 +0800

    add /ip to get ip

--- api.py
+++ api.py
@@ -4,10 +4,10 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import requests
 from tornado.options import define, options
 
 define("port", default=9000, help="run on the given port", type=int)
-#define("port", default=80, help="run on the given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
@@ -18,10 +18,22 @@ class MainHandler(tornado.web.RequestHandler):
         }
         self.write(response)
 
+class GetIpHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        ip = requests.get('http://checkip.amazonaws.com/')
+        response = {
+            "type": "app",
+            "version": "0.1",
+            "public-ip": ip.text.replace('\n', '')
+        }
+        self.write(response)
+
 def main():
     tornado.options.parse_command_line()
     application = tornado.web.Application([
         (r"/", MainHandler),
+        (r"/ip", GetIpHandler),
     #], autoreload = True)
     ])
     http_server = tornado.httpserver.HTTPServer(application)
