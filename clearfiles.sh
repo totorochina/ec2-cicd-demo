493711750d95ab64b3fea8009ad1b33a3aa4d3fd
Dave Chen <totorochina@hotmail.com>
2018-09-28 07:57:47 +0800

    add clearfiles.sh

--- clearfiles.sh
+++ clearfiles.sh
@@ -5,7 +5,8 @@ DT=$(date +'%Y%m%d-%H%M%S')
 cd /data
 shopt -s extglob
 rm *.!(log)
-find -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
+#find -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
+find -mindepth 1 -maxdepth 1 -type f -iname "*.pid" -exec rm -rf {} \;
 
 echo "clearfiles.sh executed at ${DT}" >> /data/clearfiles.log
 exit 0
