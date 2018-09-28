30c6665d0f16991d2a6eecbf2b2733f89ecdf28f
Dave Chen <totorochina@hotmail.com>
2018-09-28 15:52:12 +0800

    fixed a bug in clearfiles.sh to remove .pid

--- clearfiles.sh
+++ clearfiles.sh
@@ -6,7 +6,7 @@ cd /data
 shopt -s extglob
 rm *.!(log)
 #find -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
-find -mindepth 1 -maxdepth 1 -type f -iname "*.pid" -exec rm -rf {} \;
+find -mindepth 1 -maxdepth 1 -type f ! -iname "*.pid" -exec rm -rf {} \;
 
 echo "clearfiles.sh executed at ${DT}" >> /data/clearfiles.log
 exit 0
